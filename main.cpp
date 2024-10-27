bb806b143619c01496dfa6c07bf67e798313d11d
Gabe Susman <gabe.susman@gmail.com>
2024-10-26 20:27:19 -0600

    rudimentary cpp program and make targets

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Cat Cam Stream Service Initializing..." << std::endl;
+    std::cout << "Cat Cam Stream Service Online" << std::endl;
+    return 0;
+}
